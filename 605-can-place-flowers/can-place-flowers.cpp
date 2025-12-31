5ec85461af5221c5f5c68d9c074b6c157403394a
Dudhatra Mayank <mayankdudhatracg@gmail.com>
2025-12-31 12:12:06 +0530

    Time: 0 ms (100.00%) | Memory: 24.1 MB (40.38%) - LeetSync

---
+++ 605-can-place-flowers/can-place-flowers.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+   bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+    int k = flowerbed.size();
+
+    for (int i = 0; i < k; i++) {
+        if (flowerbed[i] == 0) {
+            int left  = (i == 0) ? 0 : flowerbed[i - 1];
+            int right = (i == k - 1) ? 0 : flowerbed[i + 1];
+
+            if (left == 0 && right == 0) {
+                flowerbed[i] = 1;
+                n--;
+                if (n == 0) return true;
+            }
+        }
+    }
+    return n <= 0;
+}
+
+};
