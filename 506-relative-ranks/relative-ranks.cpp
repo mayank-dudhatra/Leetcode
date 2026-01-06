bca1d65721f39d5620233d786dcfc9b39b407f78
Dudhatra Mayank <mayankdudhatracg@gmail.com>
2026-01-06 12:14:58 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (52.25%) - LeetSync

---
+++ 506-relative-ranks/relative-ranks.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& score) {
+        int n = score.size();
+        vector<string> result(n);
+
+        priority_queue<pair<int, int>> pq;
+
+        for(int i=0; i<n; i++){
+            pq.push({score[i], i});
+        }
+
+        int rank = 1;
+
+        while(!pq.empty()){
+            int index = pq.top().second;
+            pq.pop();
+
+            if(rank == 1) result[index] = "Gold Medal";
+            else if(rank == 2) result[index] = "Silver Medal";
+            else if(rank == 3) result[index] = "Bronze Medal";
+            else result[index] = to_string(rank);
+
+            rank++;
+        }
+
+        return result;
+    }
+};
