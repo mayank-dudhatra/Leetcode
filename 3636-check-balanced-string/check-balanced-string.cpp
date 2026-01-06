34b2d1d9dbbc65cfef4d77038916aaeaf2543092
Dudhatra Mayank <mayankdudhatracg@gmail.com>
2026-01-06 20:02:45 +0530

    Time: 3 ms (11.04%) | Memory: 8.5 MB (29.71%) - LeetSync

---
+++ 3636-check-balanced-string/check-balanced-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isBalanced(string num) {
+        int n = num.size();
+        int evensum = 0;
+        int oddsum = 0;
+
+        for(int i=0; i<n; i++){
+            if(i%2==0) {
+                evensum += num[i] - '0';
+            } else {
+                oddsum += num[i] - '0';
+            }
+        }
+
+        return oddsum == evensum;
+    }
+};
