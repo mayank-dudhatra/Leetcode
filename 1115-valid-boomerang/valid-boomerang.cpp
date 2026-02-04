81133492ec5bfc4770303ef46aece74765d008e5
Dudhatra Mayank <mayankdudhatracg@gmail.com>
2026-02-04 10:25:43 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (100.00%) - LeetSync

---
+++ 1115-valid-boomerang/valid-boomerang.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isBoomerang(vector<vector<int>>& points) {
+        int x1,x2,x3,y1,y2,y3;
+        x1 = points[0][0];
+        y1 = points[0][1];
+        x2 = points[1][0];
+        y2 = points[1][1];
+        x3 = points[2][0];
+        y3 = points[2][1];
+        return (x1 * (y2-y3) +  x2 * (y3 - y1) + x3 * (y1 - y2)) != 0;
+    }
+};
