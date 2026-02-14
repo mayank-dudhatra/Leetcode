9fef515623ed7bbd3ccb768aa45258b02ff6599b
Dudhatra Mayank <mayankdudhatracg@gmail.com>
2026-02-14 10:20:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (15.38%) - LeetSync

---
+++ 3468-find-the-encrypted-string/find-the-encrypted-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string getEncryptedString(string s, int k) {
+        int n = s.size();
+        string result = "";
+        
+        k = k % n;  
+        
+        for(int i = 0; i < n; i++) {
+            result += s[(i + k) % n];
+        }
+        
+        return result;
+    }
+};
