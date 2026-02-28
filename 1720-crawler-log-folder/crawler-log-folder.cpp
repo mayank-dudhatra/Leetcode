22d4f404150085ff6f41e27c6df7a216af108d6f
Dudhatra Mayank <mayankdudhatracg@gmail.com>
2026-03-01 02:57:16 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (82.22%) - LeetSync

---
+++ 1720-crawler-log-folder/crawler-log-folder.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int c = 0;
+
+        for (int i = 0; i < logs.size(); i++) {
+            if (logs[i] == "../") {
+                if (c != 0)
+                    c--;
+            } else if (logs[i] == "./")
+                continue;
+            else
+                c++;
+        }
+        return c;
+    }
+};
