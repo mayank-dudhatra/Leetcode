21eedc2252162599444e29a71827f3297b3bb97f
Dudhatra Mayank <mayankdudhatracg@gmail.com>
2026-03-16 10:57:04 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (98.45%) - LeetSync

--- 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.cpp
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
+    
     int numberOfSteps(int num) {
+
         int count = 0;
-        
-        while (num>0){
-            if(num%2 == 0){
-                num/=2; 
+
+        while(num>0){
+            if(num%2==0){
+                num /= 2;
                 count++;
-            } 
-            else {
-                num-=1;
+            } else {
+                num--;
                 count++;
             }
         }
