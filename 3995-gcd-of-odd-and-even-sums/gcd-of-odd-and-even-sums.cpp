a7652f4b7928c1c284505812dde4abdd20cd3a81
Dudhatra Mayank <mayankdudhatracg@gmail.com>
2026-07-16 18:41:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (46.98%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
+    int GCD(int a, int b) {
+        while(b != 0){
+            int rem = a%b;
+            a = b;
+            b = rem;
+        }
+        return a;
+    }
+
     int gcdOfOddEvenSums(int n) {
-        return n;
+        int i=0;
+        int esum = 0;
+        int osum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            esum += (2 * i - 1); 
+            osum += (2 * i);    
+        }
+
+        return GCD(esum, osum);
     }
 };
