282dd36921d76dd2aa32d71a7cf3c2607f5d2645
Dudhatra Mayank <mayankdudhatracg@gmail.com>
2026-09-19 15:13:32 +0530

    Time: 1 ms (58.47%) | Memory: 71.5 MB (12.10%) - LeetSync

---
+++ 135-candy/candy.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+
+        vector<int> res(n, 1);
+
+        for(int i = 1; i < n; i++) {
+            if(ratings[i] > ratings[i-1]) {
+                res[i] = res[i-1] + 1;
+            }
+        }
+
+        for(int j = n-2; j >= 0; j--) {
+            if(ratings[j] > ratings[j+1]) {
+                res[j] = max(res[j], res[j+1] + 1);
+            }
+        }
+
+        int sum = 0;
+
+        for(int k = 0; k < n; k++) {
+            sum += res[k];
+        }
+
+        return sum;
+    }
+};
