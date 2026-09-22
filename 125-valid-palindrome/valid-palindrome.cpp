d0fb0e441cbd1688a981c2576becf6ebe7beb351
Dudhatra Mayank <mayankdudhatracg@gmail.com>
2026-09-22 14:04:28 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (52.20%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -6,11 +6,8 @@ public:
 
         while(left < right){
             if(!isalnum(s[left])) left++;
-
             else if(!isalnum(s[right])) right--;
-
             else if( tolower(s[left]) != tolower(s[right]) ) return false;
-
             else {
                 left++;
                 right--;
